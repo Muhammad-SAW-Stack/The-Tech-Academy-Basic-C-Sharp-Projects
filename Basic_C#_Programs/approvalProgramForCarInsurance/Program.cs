487df4a1136546485292e4631a3fc74fde677426
Muhammad-SAW-Stack <109488929+Muhammad-SAW-Stack@users.noreply.github.com>
2023-01-20 14:19:15 -0500

    Update Program.cs

--- Basic_C#_Programs/approvalProgramForCarInsurance/Program.cs
+++ Basic_C#_Programs/approvalProgramForCarInsurance/Program.cs
@@ -14,29 +14,24 @@ namespace approvalProgramForCarInsurance
             //Writes "What is your age?"
             Console.WriteLine("What is your age?");
             //Keeps the window from closing
-            string yourAge = Console.ReadLine();
+            Console.ReadLine();
+            //Convert to int, declares variable
+            int yourAge = Convert.ToInt32(Console.ReadLine());          
+
             //Writes "Have you ever had a DUI?"
             Console.WriteLine("Have you ever had a DUI?");
-            string anyDUI = Console.ReadLine();           
+            //Convert to int, declares variable
+            int anyDUI = Convert.ToInt32(Console.ReadLine());          
             //Writes "How many speeding tickets do you have?"            
             Console.WriteLine("How many speeding tickets do you have?");
-            string anyTickets = Console.ReadLine();
+            //Convert to int, declares variable
+            int anyTickets = Convert.ToInt32(Console.ReadLine());
             Console.ReadLine();
-            if (anyDUI >= 1);
-            {
-                Console.WriteLine("You do not qualify");
-            }
-            if (yourAge < 15);
-            {
-                Console.WriteLine("You do not qualify");
-            }
-            if (anyTickets > 3)
-            {
-                Console.WriteLine("You do not qualify");
-            }
-            
-            
-          
-        }
+            //Declares variables
+            int a = yourAge
+            int b = anyDUI
+            int c = anyTickets  
+            Console.WriteLine(a > 15 && b < 1 && c <= 3 ); 
+          }
     }
 }
