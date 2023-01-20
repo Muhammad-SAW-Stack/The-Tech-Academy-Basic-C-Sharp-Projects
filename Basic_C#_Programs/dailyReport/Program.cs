4b52869d0f7d17233c58721dff1b6ed5f65bb714
Muhammad-SAW-Stack <109488929+Muhammad-SAW-Stack@users.noreply.github.com>
2023-01-20 02:54:55 -0500

    Add files via upload

---
+++ Basic_C#_Programs/dailyReport/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dailyReport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Writes "Person 1"
+            Console.WriteLine("Academy of Learning Career College");
+            //Keeps the window from closing
+            Console.ReadLine();
+            //Writes "Student Daily Report"
+            Console.WriteLine("Student Daily Report");
+            //Keeps window open
+            Console.ReadLine();
+            //Writes "What is your name?"
+            Console.WriteLine("What is your name?");
+            //Definest string
+            string yourName = Console.ReadLine();
+            //Keeps window open
+            Console.ReadLine();
+            Console.WriteLine("What course are you on?");
+            Console.ReadLine();
+            //Defines string
+            string yourCourse = Console.ReadLine();
+            Console.WriteLine("What page number?");
+            Console.ReadLine();
+            Console.WriteLine("Do you need help with anything?");
+            Console.ReadLine();
+            Console.WriteLine("Please answer “true” or “false”.");
+            Console.ReadLine();
+            Console.WriteLine("Were there any positive experiences you’d like to share? Please give specifics.");
+            Console.ReadLine();
+            Console.WriteLine("Is there any other feedback you’d like to provide? Please be specific.");
+            Console.ReadLine();
+            Console.WriteLine("How many hours did you study today ?");
+            Console.ReadLine();
+            Console.WriteLine("Thank you for your answers. An Instructor will respond shortly.Have a great day!");
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
