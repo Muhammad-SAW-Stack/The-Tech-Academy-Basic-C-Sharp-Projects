3e56cfeba839eca00241e80b0f4e55c5321c6afe
Muhammad-SAW-Stack <109488929+Muhammad-SAW-Stack@users.noreply.github.com>
2023-01-20 02:37:53 -0500

    Add files via upload

---
+++ Basic_C#_Programs/approvalProgramForCarInsurance/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace approvalProgramForCarInsurance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Writes "What is your age?"
+            Console.WriteLine("What is your age?");
+            //Keeps the window from closing
+            string yourAge = Console.ReadLine();
+            //Writes "Have you ever had a DUI?"
+            Console.WriteLine("Have you ever had a DUI?");
+            string anyDUI = Console.ReadLine();           
+            //Writes "How many speeding tickets do you have?"            
+            Console.WriteLine("How many speeding tickets do you have?");
+            string anyTickets = Console.ReadLine();
+            Console.ReadLine();
+            if (anyDUI >= 1);
+            {
+                Console.WriteLine("You do not qualify");
+            }
+            if (yourAge < 15);
+            {
+                Console.WriteLine("You do not qualify");
+            }
+            if (anyTickets > 3)
+            {
+                Console.WriteLine("You do not qualify");
+            }
+            
+            
+          
+        }
+    }
+}
