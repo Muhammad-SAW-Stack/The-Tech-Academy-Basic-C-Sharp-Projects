fe7a4b35af7ab35839505d66bf3ad787553db184
Muhammad-SAW-Stack <109488929+Muhammad-SAW-Stack@users.noreply.github.com>
2023-01-20 14:36:52 -0500

    Update Program.cs

--- Basic_C#_Programs/dailyReport/Program.cs
+++ Basic_C#_Programs/dailyReport/Program.cs
@@ -21,26 +21,49 @@ namespace dailyReport
             Console.ReadLine();
             //Writes "What is your name?"
             Console.WriteLine("What is your name?");
-            //Definest string
-            string yourName = Console.ReadLine();
+            //Defines string, convert to int
+            int yourName = Convert.ToInt32(Console.ReadLine());
+
             //Keeps window open
             Console.ReadLine();
+            //Writes "What course are you on?"
             Console.WriteLine("What course are you on?");
             Console.ReadLine();
-            //Defines string
-            string yourCourse = Console.ReadLine();
+            //Defines string, converts it
+            int youCourse = Convert.ToInt32(Console.ReadLine());
+            //Writes "What page number?"
             Console.WriteLine("What page number?");
+            //Keeps window open
             Console.ReadLine();
+            //Defines string, converts it
+            int pageNuber = Convert.ToInt32(Console.ReadLine());
+            //Writes "WDo you need help with anything?"
             Console.WriteLine("Do you need help with anything?");
+            //Defines string, converts it
+            int anyHelp = Convert.ToInt32(Console.ReadLine());
             Console.ReadLine();
+            //Writes "Please answer “true” or “false”."
             Console.WriteLine("Please answer “true” or “false”.");
+            //Defines string, converts it
+            //Defines string, converts it
+            int trueOrFalse = Convert.ToInt32(Console.ReadLine());
             Console.ReadLine();
+            // Writes "Were there any positive experiences you’d like to share? Please give specifics."
             Console.WriteLine("Were there any positive experiences you’d like to share? Please give specifics.");
-            Console.ReadLine();
+            //Defines string, converts it
+            int goodTimes = Convert.ToInt32(Console.ReadLine());
+            //Defines string, converts itConsole.ReadLine();
+            // Writes "Is there any other feedback you’d like to provide? Please be specific."
             Console.WriteLine("Is there any other feedback you’d like to provide? Please be specific.");
+            //Defines string, converts it
+            int anyFeedback = Convert.ToInt32(Console.ReadLine());
             Console.ReadLine();
+            //Writes "How many hours did you study today ?"
             Console.WriteLine("How many hours did you study today ?");
+            //Defines string, converts it
+            int todayStudyHours = Convert.ToInt32(Console.ReadLine());
             Console.ReadLine();
+            //Writes "Thank you for your answers. An Instructor will respond shortly.Have a great day!"
             Console.WriteLine("Thank you for your answers. An Instructor will respond shortly.Have a great day!");
 
 
