77b5370c72dbf1126b8c3d29cd87c5603bba5b33
Muhammad-SAW-Stack <109488929+Muhammad-SAW-Stack@users.noreply.github.com>
2023-01-25 14:53:47 -0500

    Add files via upload

---
+++ Basic_C#_Programs/Class Method Assignment/ToString.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Method_Assignment
+{
+    class ToString
+    {
+
+            static void Main(string[] args)
+            {
+                String s;
+
+                int num = 299;
+                s = num.ToString();
+
+                Console.WriteLine("String = " + s);
+                Console.ReadLine();
+            }
+    }
+}
