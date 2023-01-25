ce42348901a7118ad40037e2c7ec4146ab2f4a89
Muhammad-SAW-Stack <109488929+Muhammad-SAW-Stack@users.noreply.github.com>
2023-01-25 15:12:09 -0500

    Add files via upload

---
+++ Basic_C#_Programs/DateAndTime/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DateAndTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Prints "current Date and Time is : "
+            Console.Write("Current Date and Time is : ");
+            //"DateTime now" property return the current date and time.
+            DateTime now = DateTime.Now;
+            //Prints the current Date and Time.
+            Console.WriteLine(now);
+            //Keeps window open
+            Console.ReadLine();
+            //Prints "Please enter a number".
+            Console.Write("Please enter a number");
+           
+        }
+    }
+}
