800f810c726204e946225bc44512e7d36d181223
Muhammad-SAW-Stack <109488929+Muhammad-SAW-Stack@users.noreply.github.com>
2023-01-26 15:16:57 -0500

    Add files via upload

---
+++ Basic_C#_Programs/ClassesAndObjectsPart1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndObjectsPart1
+{
+    class Program
+    {
+        public void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome to the Grand Hotel and Casino. Let's start by telling me your name.");
+            string playerName = Console.ReadLine();
+            Console.WriteLine("And how much money did you bring today?");
+            int bank = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Hello, {0}. Would you like to join a game of 21 right now?");
+            string answer = Console.ReadLine().ToLower();
+            if (answer == "yes" || answer == "yeah" || answer == "y" || answer == "ya") 
+            {
+                Player player = new Player(playerName, bank);
+            }
+        }
+   }
+}
