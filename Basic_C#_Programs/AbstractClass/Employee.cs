bd8b3ed91f3689ba65d088d142ef657e9464997a
Muhammad-SAW-Stack <109488929+Muhammad-SAW-Stack@users.noreply.github.com>
2023-01-31 18:08:26 -0500

    Add files via upload

---
+++ Basic_C#_Programs/AbstractClass/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbstractClass
+{
+    public class Employee : Person
+    {
+        public override void SayName()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
