8da58ced901832fb7cf6007d86db1d512cda3096
Muhammad-SAW-Stack <109488929+Muhammad-SAW-Stack@users.noreply.github.com>
2023-01-31 14:41:13 -0500

    Add files via upload

---
+++ Basic_C#_Programs/Class Method Assignment/MathMethod.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+    class MathOperations
+    {
+        class Subtract
+        {
+        public void Main(string[] args)
+        {
+
+            // Subtract the first number by the second number.
+            decimal d1 = 1.2m;
+            decimal d2 = 2.3m;
+            decimal d3 = 4.4m;
+            decimal d4 = 6.5m;
+
+            // subtract decimal values
+            decimal n1 = decimal.Subtract(d1, d2);
+            decimal n2 = decimal.Subtract(d3, d4);
+
+            // print results
+            Console.WriteLine(n1);
+            Console.WriteLine(n2);
+        }
+                
+    }
+        
+ }
+ 
+
+
---
+++ Basic_C#_Programs/Class Method Assignment/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Method_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result;
+            int x = 5, y = 10;
+            result = (x + y);
+            Console.WriteLine("Addition Operator: " + result);
+            Console.ReadLine();
+
+
+
+
+        }
+
+    }
+}
+    
+
