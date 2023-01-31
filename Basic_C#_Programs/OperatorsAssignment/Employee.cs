c31a48b7b1e67a37178fe66da141a0f747c0ff5d
Muhammad-SAW-Stack <109488929+Muhammad-SAW-Stack@users.noreply.github.com>
2023-01-31 14:47:19 -0500

    Add files via upload

---
+++ Basic_C#_Programs/OperatorsAssignment/Employee.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OperatorsAssignment
+{   //Creates class called 'Employee'. ":" tells 'Employee' class
+    //to inherit from 'Person' class//
+    class Employee : Person
+    {
+        // The following is an Auto-Implemented Property.
+        // It's basically a shorthand way of creating properties for a class in C#,
+        // without having to define private variables
+        // for them. They are normally used when no extra logic is required
+        // when getting or setting the value of a variable.
+        public int ID { get; set; }
+        //"Operator" Overloading C# lets you overload operators to work with operands
+        // that are custom classes or structs using operators.
+        public static bool operator ==(Employee employee, Employee employee2)
+        {
+            //The if Statement. Use the if statement to specify a block of C# code to be executed
+            //if a condition is True
+            if (employee.ID == employee2.ID)
+                return true;
+            //If not true
+            else
+                //Return false
+                return false;
+        }
+        //"Operator" Overloading C# lets you overload operators to work with operands
+        // that are custom classes or structs using operators.
+        public static bool operator !=(Employee employee, Employee employee2)
+        {
+            //Return 'employee ID' does not equal 'employee ID'. 
+            return employee.ID != employee2.ID;
+        }
+        //An override method provides a new implementation of the method inherited from a base class.
+        public override bool Equals(object obj)
+        {
+            var emp = obj as Employee;
+            if (emp == null)
+                return false;
+
+            return this.ID.Equals(emp.ID);
+        }
+}
---
+++ Basic_C#_Programs/OperatorsAssignment/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace OperatorsAssignment
+{
+    class Program
+    {
+        //The Main() method is the first method which gets invoked
+        //whenever an application started and it is present in every C# executable file.
+        static void Main(string[] args)
+        {
+            -
+            Employee example = new Employee();
+
+            example.FirstName = "Mike";
+            example.LastName = "Smith";
+            example.ID = 24923;
+
+            Employee example2 = new Employee();
+            example2.FirstName = "John";
+            example2.LastName = "Doe";
+            example2.ID = 39292;
+
+            Console.ReadLine();
+        }
+        public class Person
+        {
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+        }
+    }
+}
+
