5081a460f1199a7a7fd0a87646dc13bb28b3d9f8
Muhammad-SAW-Stack <109488929+Muhammad-SAW-Stack@users.noreply.github.com>
2023-01-31 19:53:02 -0500

    Add files via upload

---
+++ Basic_C#_Programs/Console App String and Integers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Console_App_String_and_Integers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please type a number you wish to divide the numbers in the list by.");
+            string divideBy = Console.ReadLine();
+            Convert.ToInt32(divideBy);
+            int[] numberArray = { 48, 72, 14, 88 };
+            foreach (var item in numberArray)
+            {
+                var result = item / divideBy;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
