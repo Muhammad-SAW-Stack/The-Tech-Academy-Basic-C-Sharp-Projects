44668079425158f9944e7ea3c6ae8f80d2a7a082
Muhammad-SAW-Stack <109488929+Muhammad-SAW-Stack@users.noreply.github.com>
2023-02-01 20:56:24 -0500

    Add files via upload

---
+++ Basic_C#_Programs/Calling Methods/MathOperations.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace MathOperations
+{
+    public class Operations
+    {
+        // Method that performs addition operation on the given number
+        public int Addition(int num1)
+        {
+            // Adds 10 to the given number
+            return num1 + 14;
+        }
+
+        // Method that performs subtraction operation on the given number
+        public int Subtraction(int num2)
+        {
+            // Subtracts 5 from the given number
+            return num2 - 6;
+        }
+
+        // Method that performs multiplication operation on the given number
+        public int Multiplication(int num3)
+        {
+            // Multiplies the given number by 2
+            return num3 * 8;
+        }
+    }
+}
---
+++ Basic_C#_Programs/Calling Methods/Program.cs
@@ -0,0 +1,36 @@
+﻿using MathOperations;
+using System;
+
+namespace Calling_Methods
+{
+
+    class Program 
+    {
+
+        static void Main(string[] args)
+        {
+            // Instantiates the Operations class
+            Operations mathOps = new Operations();
+            // Prints to the screen "Enter a number:".
+            Console.WriteLine("Enter a number: ");
+            // Saves and converts the users input.
+            int userInput = int.Parse(Console.ReadLine());
+            // Calls the Addition method, passes the user input as an argument, and stores the result in a variable.
+            int result1 = mathOps.Addition(userInput);
+            // Calls the Subtraction method, passes the user input as an argument, and stores the result in a variable.
+            int result2 = mathOps.Subtraction(userInput);
+            // Calls the Multiplication method, passes the user input as an argument, and stores the result in a variable.
+            int result3 = mathOps.Multiplication(userInput);
+            // Prints to the screen "After addition, the result is:" the with the added result displayed.
+            Console.WriteLine("After addition, the result is: " + result1);
+            // Prints to the screen "After subtraction, the result is:" the with the subtracted result displayed.
+            Console.WriteLine("After subtraction, the result is: " + result2);
+            // Prints to the screen "After multiplication, the result is:" the with the multiplied result displayed.
+            Console.WriteLine("After multiplication, the result is: " + result3);
+            // This causes the window to stay open until a key is pressed.
+            Console.ReadKey();
+        }
+    }
+}       
+
+                
