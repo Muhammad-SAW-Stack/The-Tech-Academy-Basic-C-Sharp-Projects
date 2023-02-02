3e479c1ef9a0c56fe21cc75e7b3c8e196d5298b3
Muhammad-SAW-Stack <109488929+Muhammad-SAW-Stack@users.noreply.github.com>
2023-02-02 16:29:05 -0500

    Add files via upload

---
+++ Basic_C#_Programs/MainMethodAssignment/MathOperations.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MainMethodAssignment
+{
+    class MathOperations
+    {
+        // The "Calculate" method is a method in a C# class called 'MathOperations'.
+        // The method takes in a single parameter of the specified type, performs a math operation on it, and returns the result as an int.
+        public int Calculate(int num)
+        {
+            // Performs a math operation (addition).
+            int result = num + 25;
+            return result;
+        }
+        // The "Calculate" method is a method in a C# class called 'MathOperations'.
+        // The method takes in a single parameter of the specified type, performs a math operation on it, and returns the result as an int.
+        public int Calculate(decimal num)
+        {
+            // This code performs a different math operation (multiplication).
+            int result = (int)(num * 25);
+            return result;
+        }
+        // The "Calculate" method is a method in a C# class called 'MathOperations'.
+        // The method takes in a single parameter of the specified type, performs a math operation on it, and returns the result as an int.
+        public int Calculate(string num)
+        {
+            // Converts the string to an integer.
+            int intNum = int.Parse(num);
+            // This code performs a different math operation (subtraction).
+            int result = intNum - 25;
+            // Returns results. 
+            return result;
+        }
+    }
+}
---
+++ Basic_C#_Programs/MainMethodAssignment/Program.cs
@@ -0,0 +1,26 @@
+﻿using MainMethodAssignment;
+using System;
+
+namespace Math
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // This code instantiates the class.
+            MathOperations operations = new MathOperations();
+            // Calls the method that takes an integer and displays the result.
+            int result1 = operations.Calculate(5);
+            // Prints to the screen "Result 1: 'result'".
+            Console.WriteLine("Result 1: " + result1);
+            // Calls the method that takes a decimal and displays the result.
+            int result2 = operations.Calculate(5.5m);
+            // Prints to the screen "Result 2: 'result2'".
+            Console.WriteLine("Result 2: " + result2);
+            // Calls the method that takes a string and displays the result.
+            int result3 = operations.Calculate("15");
+            // Prints to the screen "Result 3: 'result3'".
+            Console.WriteLine("Result 3: " + result3);
+        }
+    }
+}
