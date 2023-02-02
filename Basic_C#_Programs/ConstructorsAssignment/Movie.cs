965f6b122a6b74538f20b0748a7d1fd1c8a81302
Muhammad-SAW-Stack <109488929+Muhammad-SAW-Stack@users.noreply.github.com>
2023-02-02 00:21:51 -0500

    Add files via upload

---
+++ Basic_C#_Programs/ConstructorsAssignment/Movie.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    
+    class Movie
+    {
+        // Creates a public const variable for the minimum length of a movie in minutes as defined by the Screen Actors Guild and The Academy of Motion Picture Arts and Sciences.
+        public const int MinLength1 = 60;
+        public const int MinLength2 = 40;
+        // Declares the movie's title.
+        public string Title { get; set; }
+        // Declares the movie's director.
+        public string Director { get; set; }
+        // Declares the movies length in minutes.
+        public int Length { get; set; }
+        // Creates the first constuctor with two parameters, title and director. 
+        public Movie(string title, string director)
+        {
+            Title = title;
+            Director = director;
+        }
+        // Creates the second constructor with three parameters, title, director, and length.
+        public Movie(string title, string director, int length) : this(title, director)
+        {
+            Length = length;
+        }
+    }
+}   
---
+++ Basic_C#_Programs/ConstructorsAssignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using ConsoleApp;
+using System;
+
+class Program
+{
+        static void Main(string[] args)
+        {
+            // Declares a variable using the "new" keyword. 
+            var movie = new Movie("A Clockwork Orange", "Stanley Kubrick", 136);
+            // Prints to the screen "The Academy of Motion Picture Arts and Sciences defines a feature as a film that runs for more than: 'Movie.MinLength2' minutes".
+            Console.WriteLine("The Academy of Motion Picture Arts and Sciences defines a feature as a film that runs for more than: " + Movie.MinLength2 + " minutes");
+            // Prints to the screen "However, the Screen Actors Guild asserts that a feature's running time is:  'Movie.MinLength1' minutes".
+            Console.WriteLine("However, the Screen Actors Guild asserts that a feature's running time is: " + Movie.MinLength1 + " minutes");
+            // Prints to the screen the movie title.
+            Console.WriteLine("Title: " + movie.Title);
+            // Prints to the screen the movie director.
+            Console.WriteLine("Director: " + movie.Director);
+            // Prints to the screen the movie length.
+            Console.WriteLine("Length: " + movie.Length + " minutes");
+            // Keeps the window from closing.
+            Console.ReadLine();
+        }
+
+}
