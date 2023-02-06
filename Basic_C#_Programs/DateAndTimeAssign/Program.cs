2a228844abf042c111a1caadc0897e4d483539f5
Muhammad-SAW-Stack <109488929+Muhammad-SAW-Stack@users.noreply.github.com>
2023-02-06 00:15:36 -0500

    Add files via upload

---
+++ Basic_C#_Programs/DateAndTimeAssign/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Prints to the screen the current date and time.
+            Console.WriteLine("Current Date and Time: " + DateTime.Now);
+
+            // Prints to the screen "Enter a number".
+            Console.WriteLine("Enter a number:");
+            int hours = int.Parse(Console.ReadLine());
+
+            // Calculates and displays the time after X hours
+            DateTime futureTime = DateTime.Now.AddHours(hours);
+            // Prints to the screen "Time after 'X hours' 'futureTime'.
+            Console.WriteLine("Time after " + hours + " hours: " + futureTime);
+        }
+    }
+}
