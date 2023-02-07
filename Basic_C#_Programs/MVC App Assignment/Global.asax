bb4405107dab260f8e6a9efd843f807f9091047f
Muhammad-SAW-Stack <109488929+Muhammad-SAW-Stack@users.noreply.github.com>
2023-02-07 14:38:51 -0500

    Add files via upload

---
+++ Basic_C#_Programs/MVC App Assignment/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVC_App_Assignment.MvcApplication" Language="C#" %>
---
+++ Basic_C#_Programs/MVC App Assignment/Models/Instructor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_App_Assignment.Models
+{
+    public class Instructor
+    {
+        internal int id;
+
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
